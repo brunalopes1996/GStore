0fbce1980b160ffe6a390d7cff50e8d1bc977e59
brunalopes1996 <bruuna.oliveiraa1@gmail.com>
2025-02-14 20:35:50 -0300

    Continuando o projeto GStore

--- GStore/Models/Produto.cs
+++ GStore/Models/Produto.cs
@@ -1,13 +1,37 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace GStore.Models;
 
+[Table("produto")]
     public class Produto
     {
-        public int Id { get; set; }    
+        [Key]
+         public int Id { get; set; }  
+
+        [Required(ErrorMessage = "Por favor, informe a Categoria")]
         public int CategoriaId { get; set; }
+       
+        [ForeignKey(nameof(CategoriaId))]
         public Categoria Categoria { get; set; }
+
+        [Required(ErrorMessage = "Por favor, informe o Nome")]
+        [StringLength(60, ErrorMessage = "O Nome deve possuir no máximo 60 caracteres")]
         public string Nome { get; set; }
+
+        [Display(Name = "Descrição", Prompt = "Descrição")]
+        [StringLength(1000, ErrorMessage = "O Descrição deve possuir no máximo 1000 caracteres")]
         public string Descricao { get; set; }
-        public int Qtde { get; set; }
-        public decimal ValorCusto { get; set; }
+
+        [Required]
+        [Range(0, int.MaxValue)]
+        public int Qtde { get; set; } = 0;
+
+        [Range(0, double.MaxValue)]
+        [Column(TypeName = "numeric(10,2)")]
+        public decimal ValorCusto { get; set; } = 0;
+
+        [Range(0, double.MaxValue)]
+        [Column(TypeName = "numeric(10,2)")]
         public decimal ValorVenda { get; set; }
     }
--- GStore/Program.cs
+++ GStore/Program.cs
@@ -1,7 +1,24 @@
+using GStore.Data;
+using GStore.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Conexão com o banco de dados
+string conexao = builder.Configuration.GetConnectionString("GStoreConn");
+builder.Services.AddDbContext<AppDbContext>(
+    options => options.UseMySQL(conexao)
+);
+
+// Configuração do Identity
+builder.Services.AddIdentity<Usuario, IdentityRole>(
+    options => options.SignIn.RequireConfirmedEmail = false
+).AddEntityFrameworkStores<AppDbContext>()
+.AddDefaultTokenProviders();
 
 var app = builder.Build();
 
