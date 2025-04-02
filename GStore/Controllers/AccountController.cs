870719796baa304d39a277106b557f7fcc2ed3dd
brunalopes1996 <bruuna.oliveiraa1@gmail.com>
2025-04-02 20:34:50 -0300

    Terminei de arrumar o cógido, só falta centralizar a tela de login

--- GStore/Controllers/AccountController.cs
+++ GStore/Controllers/AccountController.cs
@@ -1,36 +1,100 @@
+using System.Net.Mail;
+using System.Security.Claims;
 using GStore.Models;
 using GStore.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GStore.Controllers;
-    public class AccountController : Controller
+public class AccountController : Controller
+{
+    private readonly ILogger<AccountController> _logger;
+    private readonly SignInManager<Usuario> _signInManager;
+    private readonly UserManager<Usuario> _userManager;
+    private readonly IWebHostEnvironment _host;
+
+    public AccountController(
+        ILogger<AccountController> logger,
+        SignInManager<Usuario> signInManager,
+        UserManager<Usuario> userManager,
+        IWebHostEnvironment host
+    )
     {
-        private readonly ILogger<AccountController> _logger;
-        private readonly SignInManager<Usuario> _signInManager;
-        private readonly UserManager<Usuario> _userManager;
-        private readonly IWebHostEnvironment _host;
+        _logger = logger;
+        _signInManager = signInManager;
+        _userManager = userManager;
+        _host = host;
+    }
 
-        public AccountController(
-            ILogger<AccountController> logger,
-            SignInManager<Usuario> signInManager,
-            UserManager<Usuario> userManager,
-            IWebHostEnvironment host
-        )
+    [HttpGet]
+    public IActionResult Login(string returnUrl)
+    {
+        LoginVM login = new()
         {
-            _logger = logger;
-            _signInManager = signInManager;
-            _userManager = userManager;
-            _host = host;
-        }
+            UrlRetorno = returnUrl ?? Url.Content("~/")
+        };
+        return View(login);
+    }
 
-        [HttpGet]
-        public IActionResult Login(string returnUrl)
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Login(LoginVM login)
+    {
+        if (ModelState.IsValid)
         {
-            LoginVM login = new()
+            string userName = login.Email;
+            if (IsValidEmail(login.Email))
             {
-                UrlRetorno = returnUrl ?? Url.Content("~/")
-            };
-            return View(login);
+                var user = await _userManager.FindByEmailAsync(login.Email);
+                if (user != null)
+                    userName = user.UserName;
+            }
+
+            var result = await _signInManager.PasswordSignInAsync(
+                userName, login.Senha, login.Lembrar, lockoutOnFailure: true
+            );
+
+            if (result.Succeeded)
+            {
+                _logger.LogInformation($"Usuário {login.Email} acessou o sistema");
+                return LocalRedirect(login.UrlRetorno);
+            }
+
+            if (result.IsLockedOut)
+            {
+                _logger.LogWarning($"Usuário {login.Email} está bloqueado");
+                ModelState.AddModelError("", "Sua conta está bloqueada, aguarde alguns minutos e tente novamente!!");
+            }
+            else
+            if (result.IsNotAllowed)
+            {
+                _logger.LogWarning($"Usuário {login.Email} não confirmou sua conta");
+                ModelState.AddModelError(string.Empty, "Sua conta não está confirmada, verifique seu email!!");
+            }
+            else
+                ModelState.AddModelError(string.Empty, "Usuário e/ou Senha Inválidos!!!");
+        }
+        return View(login);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Logout()
+    {
+        _logger.LogInformation($"Usuário {ClaimTypes.Email} fez logoff");
+        await _signInManager.SignOutAsync();
+        return RedirectToAction("Index", "Home");
+    }
+    public bool IsValidEmail(string email)
+    {
+        try
+        {
+            MailAddress m = new(email);
+            return true;
+        }
+        catch (FormatException)
+        {
+            return false;
         }
     }
+}
